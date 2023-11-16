51d8a449e188f39273132c2a069b291ae80bb9d3
LaurentV1 <145879932+LaurentV1@users.noreply.github.com>
2023-11-16 08:34:47 +0100

    Delete over-privilege/app directory

--- over-privilege/app/src/main/java/com/example/benignapp/StringLibrary.java
+++
@@ -1,7 +0,0 @@
-package com.example.benignapp;
-
-public class StringLibrary {
-    public static String reverseString(String input) {
-        return new StringBuilder(input).reverse().toString();
-    }
-}

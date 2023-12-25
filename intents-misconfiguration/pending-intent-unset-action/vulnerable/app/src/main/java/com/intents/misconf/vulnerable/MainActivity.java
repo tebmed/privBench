4fea5eb7513ee60260359f20797a20200e58ac37
Mohammed El Amin TEBIB <33195777+tebmed@users.noreply.github.com>
2023-12-25 02:04:03 +0100

    pending intent creation with unset action (#24)

---
+++ intents-misconfiguration/pending-intent-unset-action/vulnerable/app/src/main/java/com/intents/misconf/vulnerable/MainActivity.java
@@ -0,0 +1,40 @@
+package com.intents.misconf.vulnerable;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an empty PendingIntent
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        MainActivity.this,
+                        0,
+                        new Intent(),
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+                try {
+                    // Trigger the PendingIntent
+                    pendingIntent.send();
+                } catch (PendingIntent.CanceledException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}

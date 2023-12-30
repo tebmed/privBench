cc160182b902165a630d3470cfa83dcab2209cb0
tebmed <lamin.teb94@gmail.com>
2023-12-30 01:08:57 +0100

    java reflection - class.forname() pattern

---
+++ overuse-permissions/reflective-appi-calls/phone-call-api-java/vulnerable/app/src/main/java/com/overpem/java/reflection/vulnerable/MainActivity.java
@@ -0,0 +1,83 @@
+package com.overpem.java.reflection.vulnerable;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CALL_PHONE = 1;
+    private EditText phoneNumberEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneNumberEditText = findViewById(R.id.phone_number_edit_text);
+
+        // Button click listener to initiate the call
+        findViewById(R.id.call_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
+            }
+        });
+
+        // Check for permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    REQUEST_CALL_PHONE);
+        }
+    }
+
+    // Handle permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL_PHONE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with the call if phone number is not empty
+                if (!phoneNumberEditText.getText().toString().isEmpty()) {
+                    makePhoneCall();
+                } else {
+                    Toast.makeText(this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Method to make the phone call
+    private void makePhoneCall() {
+        try {
+            // Using Class.forName to access Intent.ACTION_CALL
+            Class<?> c = Class.forName("android.content.Intent");
+            Intent intent = new Intent((String) c.getField("ACTION_CALL").get(null));
+            String phoneNumber = phoneNumberEditText.getText().toString().trim();
+            if (!phoneNumber.isEmpty()) {
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+            }
+        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}

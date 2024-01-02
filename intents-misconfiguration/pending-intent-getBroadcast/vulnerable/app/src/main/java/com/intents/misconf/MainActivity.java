71ddf9b9e4ad8f5ea59e1017fcff4ed48647bcae
Mohammed El Amin TEBIB <33195777+tebmed@users.noreply.github.com>
2024-01-02 01:18:04 +0100

    unset intent action with getreceiver (#35)

---
+++ intents-misconfiguration/pending-intent-getBroadcast/vulnerable/app/src/main/java/com/intents/misconf/MainActivity.java
@@ -0,0 +1,48 @@
+package com.intents.misconf;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TimePicker alarmTimePicker;
+    private AlarmManager alarmManager;
+    private PendingIntent alarmPendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarmTimePicker = findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        Button setAlarmButton = findViewById(R.id.setAlarm);
+        setAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm();
+            }
+        });
+    }
+
+    private void setAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
+        calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        alarmPendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmPendingIntent);
+    }
+}
